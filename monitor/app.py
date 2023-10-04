9c2f656251107c38da3c398d1b0030f30cd765f7
Yusuf Mayet <jojo786@gmail.com>
2023-10-04 20:55:00 +0200

    clean up Lambda

--- monitor/app.py
+++ monitor/app.py
@@ -3,14 +3,14 @@ from aws_lambda_powertools import Metrics
 from aws_lambda_powertools.metrics import MetricUnit
 import urllib.parse
 
-metrics = Metrics(namespace="BackupsMonitoring")
+metrics = Metrics(namespace="BackupsMonitoring") #the CloudWatch namespace
 
 @metrics.log_metrics  # ensures metrics are flushed upon request completion/failure
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
     print("Start processing S3 Event")
     
-    filename = urllib.parse.unquote_plus(urllib.parse.unquote(event['Records'][0]['s3']['object']['key']))  #S3 event contains document name in URL encoding, needs to be decoded - https://github.com/aws-samples/amazon-textract-enhancer/issues/2
-    print ("Customer Backup: " + filename)
+    file_name = urllib.parse.unquote_plus(urllib.parse.unquote(event['Records'][0]['s3']['object']['key']))  #S3 event contains file name in URL encoding, needs to be decoded - https://github.com/aws-samples/amazon-textract-enhancer/issues/2
+    print ("Backup file: " + file_name)
     
-    customer_name = filename.split("/")[0] #the prefix, i.e. the folder, is the customer name
-    metrics.add_metric(name=customer_name, unit=MetricUnit.Count, value=1)
+    system_name = file_name.split("/")[0] #the prefix, i.e. the folder which is the metric you want you monitor
+    metrics.add_metric(name=system_name, unit=MetricUnit.Count, value=1) #each time a backup file is copied to S3, push a custom metric with the name of folder as the system name, with the value of 1.
